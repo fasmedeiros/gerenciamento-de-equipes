75e96630d300d5759220795bb806f318cd67905c
Fabrício de Medeiros <gannba@hotmail.com>
2025-01-06 19:27:45 -0300

    Delete vendor directory

--- vendor/jurager/teams/src/Events/TeamCreated.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Jurager\Teams\Events;
-
-class TeamCreated extends TeamEvent
-{
-    //
-}
--- vendor/jurager/teams/src/Events/TeamUpdated.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Jurager\Teams\Events;
-
-class TeamUpdated extends TeamEvent
-{
-    //
-}
